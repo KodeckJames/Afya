e57625d21e768ff45ea9b52e9d2f7bff3f958e85
Kodeck James <kodeckjames095@gmail.com>
2024-11-06 12:47:03 +0300

    Re-initialized NativeWind

---
+++ client/afya/src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { StyleSheet } from 'nativewind'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View className=" flex-1 bg-black">
+      <Text className=" text-white">Welcome to Afya, how can I help you?</Text>
+    </View>
+  )
+}
+
+export default Index
