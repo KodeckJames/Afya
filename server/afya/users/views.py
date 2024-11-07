4dce26c3b86b20a99834f9da846a01b67e4e5f65
addymwenda12 <muneneaddy@gmail.com>
2024-11-07 18:10:29 +0300

    Created views to handle user-related views

--- server/afya/users/views.py
+++ server/afya/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import CustomUser
 
-# Create your views here.
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
